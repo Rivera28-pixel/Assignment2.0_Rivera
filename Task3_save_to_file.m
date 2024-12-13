0c5cec7d70a10289de70609365db0ea10ad0bf21
Rivera28-pixel <br18716@gmail.com>
2024-12-12 21:32:02 -0500

    Update Task3_save_to_file.m

--- Task3_save_to_file.m
+++ Task3_save_to_file.m
@@ -1,3 +1,9 @@
+% Blanca Rivera
+% rivera222@rowan.edu
+% Matlab Programming 
+% Assignment 2
+% Task 3: Weather Data Analysis
+% 10/18/2024
 
 function res= save_to_file(file_name,data)
     fileID= fopen(file_name,'w'); %creates function that will save the avg and total of the data from the other file and display it in the summary.txt
